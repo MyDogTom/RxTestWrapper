126cfce2410550a7db0499a33bc1b153cfcd578a
Svyatoslav Chatchenko <mydogtom@gmail.com>
2016-10-25 02:33:42 +0200

    next step

---
+++ rxtestwrapper-kotlin/src/test/kotlin/com/github/mydogtom/rxtestwrapper/ExampleTest.kt
@@ -0,0 +1,9 @@
+package com.github.mydogtom.rxtestwrapper
+
+import org.junit.Test
+
+class ExampleTest{
+    @Test fun name() {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
